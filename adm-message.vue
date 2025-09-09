8d4ef5e61dbc05c599881732c6a09e7fbb0454f7
God's face <god-s_face@Naroah-Mac.lan>
2025-09-09 11:12:00 +0800

    完成adm-message组件

---
+++ adm-message.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="message-wrap">
+    <adm-row :isThin="true" :height="width">
+      <slot></slot>
+    </adm-row>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps } from "vue";
+import { screen, getRpx } from "./adm";
+
+const props = defineProps({
+  isGrop: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+});
+const width = computed(() =>
+  props.isGrop ? 684.21 : getRpx(screen.value.width),
+);
+</script>
+
+<style scoped lang="scss">
+@import "./adm.scss";
+.message-wrap {
+  position: absolute;
+  transform: rotate(-90deg);
+  transform-origin: right top;
+  margin-left: -$height-mini;
+}
+</style>
