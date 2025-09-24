9398ccb660a40fa0af8ec6abc8e3102affe3a806
God's face <god-s_face@Naroah-Mac.lan>
2025-09-24 10:59:20 +0800

    重构adm-tab组件

--- adm-tab.vue
+++ adm-tab.vue
@@ -1,48 +1,29 @@
 <template>
-	<adm-fill :fill-text="fillText" style="position: fixed;">
-		<view v-for="(item,index) in tabArr" :key="index">
-			<adm-item :is-rev="current == index" @click="inClick(index)">{{item}}</adm-item>
-		</view>
-	</adm-fill>
+  <adm-fill :fill-text="props.text" style="position: fixed">
+    <view v-for="(item, index) in props.tabArr" :key="index">
+      <adm-item :is-rev="props.current == index" @click="emit('click', index)">
+        {{ item }}
+      </adm-item>
+    </view>
+  </adm-fill>
 </template>
 
-<script lang="ts">
-  import adm from '../sdk/adm';
-	export default {
-		name: "admTab",
-		emits: ['clickItem'],
-		props: {
-			fillText: {
-				type: String,
-				default: '委員會',
-				required: false
-			},
-			tabArr: {
-				type: Array,
-				required: true
-			},
-			current: {
-				type: Number,
-				default: 0,
-				required: false
-			}
-		},
-		data() {
-			return {
-
-			};
-		},
-    mounted() {
-      adm.tab = true
-    },
-		methods: {
-			inClick(index : number) {
-				this.$emit("clickItem", index)
-			}
-		}
-	}
+<script setup lang="ts">
+const emit = defineEmits(["click"]);
+const props = defineProps({
+  text: {
+    type: String,
+    default: "委員會",
+    required: false,
+  },
+  tabArr: {
+    type: Array,
+    required: true,
+  },
+  current: {
+    type: Number,
+    default: 0,
+    required: false,
+  },
+});
 </script>
-
-<style>
-
-</style>
